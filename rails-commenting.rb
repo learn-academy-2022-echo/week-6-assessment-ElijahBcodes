4d838a008c205c31fce3b2a8073e7e4f086c21fa
Elijah Barreto <51147388+ElijahBcodes@users.noreply.github.com>
2022-09-26 15:54:28 -0700

    Revert "week-6"

--- rails-commenting.rb
+++ rails-commenting.rb
@@ -6,25 +6,25 @@
 
 # FILE: app/controller/blog_posts_controller.rb
 
-# ---1)BlogPostsController is a child of app controller
+# ---1)
 class BlogPostsController < ApplicationController
   def index
-    # ---2) method to index each blog post get instance of posts and make it equal to all, will show all when called
+    # ---2)
     @posts = BlogPost.all
   end
 
-  # ---3) Show method/route. With the index method we are able to use params to show a particular blog post, using active record call /params to index the correct post we are refrencing.
+  # ---3)
   def show
     @post = BlogPost.find(params[:id])
   end
 
-  # ---4) New method/route to make display from to user. Now when you declare BlogPost.new can declare a new instance of a blog post that can be stored to db
+  # ---4)
   def new
     @post = BlogPost.new
   end
 
   def create
-    # ---5) Restful route that submist user data to db. The instance of post is equal to BlogPost.create passing params if its valid passing post request 
+    # ---5)
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -34,18 +34,18 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    # ---6)Method that allows to edit a post. Using params and the id to edit a post
+    # ---6)
     @post = BlogPost.find(params[:id])
   end
 
   def update
     @post = BlogPost.find(params[:id])
-    # ---7)The update changes item in db using strong params
-    @post.update(blog_post_params)    #at the instace of update use params
-    if @post.valid?                   #if the post is valid
-      redirect_to blog_post_path(@post) #route to send a patch request 
+    # ---7)
+    @post.update(blog_post_params)
+    if @post.valid?
+      redirect_to blog_post_path(@post)
     else
-      redirect_to edit_blog_post_path #not valid send back to edit path
+      redirect_to edit_blog_post_path
     end
   end
 
@@ -54,15 +54,15 @@ class BlogPostsController < ApplicationController
     if @post.destroy
       redirect_to blog_posts_path
     else
-      # ---8)Send back to the path(route) to post and item will not be destroyed
+      # ---8)
       redirect_to blog_post_path(@post)
     end
   end
 
-  # ---9)Private is a keyword in Ruby that restrics scope of wher method is called. Ony meant to be used as arguments for controller methods. By calling them private we ensure they can't be called anywher else in program.
+  # ---9)
   private
   def blog_post_params
-    # ---10)Making use of strong params  provid control over what info is passed into db. Here allowing changes to title and content
+    # ---10)
     params.require(:blog_post).permit(:title, :content)
   end
 end
